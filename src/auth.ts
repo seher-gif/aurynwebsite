2beea2ca2a687434345168ae9666fff2ec085cc2
Seher Ulusoy <seher@auryndijital.com>
2025-11-26 15:06:07 +0300

    Fix: Add trustHost: true to auth config

--- src/auth.ts
+++ src/auth.ts
@@ -17,6 +17,7 @@ async function getUser(email: string) {
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
+    trustHost: true,
     providers: [
         Credentials({
             async authorize(credentials) {
